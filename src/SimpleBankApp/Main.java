00a9bdffc956852bdf7f778735a1385b424713ea
Dhanush S <dhanushseenivasan0403@gmail.com>
2024-02-20 20:52:31 +0530

    to run the application

--- src/SimpleBankApp/Main.java
+++ src/SimpleBankApp/Main.java
@@ -1,7 +1,6 @@
-import com.dhanush.Account;
+package SimpleBankApp;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args) {
         Account DhanushAccount = new Account("12345",0.0,"Dhanush","xyz@gmail.com","123456789");
